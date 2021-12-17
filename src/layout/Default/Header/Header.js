328ca0f272995508539ea6f9715fcd2d8d063e59
yymin1022 <yymin1022@gmail.com>
2021-12-17 14:41:57 +0900

    Update Header.js : Apply CECOM Logo

--- src/layout/Default/Header/Header.js
+++ src/layout/Default/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-import CecomLogo from "../../image/cecom_logo.png";
+import CecomLogo from "../../../image/cecom_logo.png";
 
 import "./Header.css"
 
