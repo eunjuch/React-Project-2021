ab3acece614e73e490cf02e0a8db00936e4a4d48
yymin1022 <yymin1022@gmail.com>
2021-12-25 19:29:21 +0900

    Update Board.js : Add API Server Connection

--- src/route/Board/Board.js
+++ src/route/Board/Board.js
@@ -3,6 +3,23 @@ import React from "react";
 import "./Board.css";
 
 const Board = () => {
+    useEffect(() => {
+        axios.post("https://api.cecom.dev/getPost"
+        ).then(
+            resultOK = (response) => {
+                console.log(response);
+            }
+        ).catch(
+            resultError = (error) => {
+                console.log(error);
+            }
+        );
+
+        return () => {
+            
+        }
+    }, []);
+
     return(
         <div id="divBoard">
             Board Page
