765582232b55accf34907b31231ca13a73c8d4f0
yymin1022 <yymin1022@gmail.com>
2021-12-30 13:16:15 +0900

    Update App.js : Add PostView / PostWrite Page

---
+++ src/route/PostWrite/PostWrite.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./PostWrite.js"
+
+const PostWrite = (props) => {
+    return(
+        <div>
+            PostWrite
+        </div>
+    )
+}
+
+export default PostWrite;
