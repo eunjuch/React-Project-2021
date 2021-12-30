b30f44ed3d52730cc875114b275bced7ed531b7c
yymin1022 <yymin1022@gmail.com>
2021-12-30 14:35:59 +0900

    Update PostView.js : Add Backend Processing

--- src/route/PostView/PostView.js
+++ src/route/PostView/PostView.js
@@ -5,9 +5,37 @@ import "./PostView.js"
 const PostView = (props) => {
     let postID = props.match.params.postID;
 
+    const [postData, setPostData] = useState({
+        "RESULT": {
+            "RESULT_CODE": 100,
+            "RESULT_MSG": "NOT LOADED"
+        },
+        "DATA": {
+            "author": "",
+            "content": "",
+            "title": "게시글을 불러오는 중입니다..."
+        }
+    });
+
+    useEffect(() => {
+        axios.post("https://api.cecom.dev/getPost",
+            {
+                "postID": postID
+            }
+        ).then(
+            function resultOK(response) {
+                setPostData(response.data);
+            }
+        ).catch(
+            function resultError (error) {
+                console.log(error);
+            }
+        );
+    }, []);
+
     return(
         <div>
-            PostView {postID}
+            PostView {postData}
         </div>
     )
 }
